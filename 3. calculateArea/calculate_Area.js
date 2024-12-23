e768e20671f94b776c930ea6972b880c53483d91
Nicktyranov <nicktyranov@gmail.com>
2024-12-23 15:48:49 -0500

    laba-3-full

--- 3. calculateArea/calculate_Area.js
+++ 3. calculateArea/calculate_Area.js
@@ -9,3 +9,15 @@ function calculateArea() {
         document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
     } 
 }
+
+function calculate(){
+    let x1 = +document.getElementById('amount1').value
+    let x2 = +document.getElementById('amount2').value
+    let x3 = +document.getElementById('amount3').value
+
+    let rez = x1 + x2 + x3;
+    console.log(`The sum is: ${rez}`)
+    if (rez){
+        document.getElementById('result2').innerText = `The sum is: ${rez}`;
+    }
+}
